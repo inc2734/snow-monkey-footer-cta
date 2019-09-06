8890869ca32b6668e7e3c62a91f78d251e39f119
Takashi Kitajima <inc@2inc.org>
2019-09-06 09:43:46 +0900

    fix #5

--- templates/footer-cta/primary-btn.php
+++ templates/footer-cta/primary-btn.php
@@ -7,13 +7,26 @@
 
 $label = get_theme_mod( 'footer-cta-primary-btn-label' );
 $url   = get_theme_mod( 'footer-cta-primary-btn-url' );
-$blank = get_theme_mod( 'footer-cta-primary-btn-blank' ) ? 'blank' : 'self';
 
 if ( ! $url || ! $label ) {
 	return;
 }
+
+$attributes = [];
+if ( get_theme_mod( 'footer-cta-primary-btn-blank' ) ) {
+	$attributes = [
+		'target' => '_blank',
+		'rel'    => 'noopener',
+	];
+}
 ?>
 
-<a class="c-btn c-btn--full p-footer-cta__primary-btn" href="<?php echo esc_url( $url ); ?>" target="<?php echo esc_attr( $blank ); ?>">
+<a
+	class="c-btn c-btn--full p-footer-cta__primary-btn"
+	href="<?php echo esc_url( $url ); ?>"
+	<?php foreach ( $attributes as $attribute => $value ) : ?>
+		<?php echo esc_html( $attribute ); ?>="<?php echo esc_attr( $value ); ?>"
+	<?php endforeach; ?>
+>
 	<?php echo esc_html( $label ); ?>
 </a>
