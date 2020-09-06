0a32858269d0385fe4a8cdd0d8f79dd6de4508a1
Takashi Kitajima <inc@2inc.org>
2020-09-06 18:00:57 +0900

    Using snow_monkey_template_part_root_hierarchy_{slug}

--- App/Controller/Front.php
+++ App/Controller/Front.php
@@ -12,11 +12,23 @@ use Framework\Helper;
 class Front {
 
 	public function __construct() {
+		add_action(
+			'after_setup_theme',
+			function() {
+				foreach ( Helper::glob_recursive( SNOW_MONKEY_FOOTER_CTA_PATH . '/templates' ) as $file ) {
+					$slug = rtrim( str_replace( SNOW_MONKEY_FOOTER_CTA_PATH . '/templates/', '', $file ), '.php' );
+					add_filter(
+						'snow_monkey_template_part_root_hierarchy_' . $slug,
+						[ $this, '_template_part_root_hierarchy' ]
+					);
+				}
+			}
+		);
+
 		add_action( 'wp_enqueue_scripts', [ $this, '_wp_enqueue_scripts' ] );
 		add_action( 'inc2734_wp_customizer_framework_load_styles', [ $this, '_load_styles' ], 11 );
 
-		add_filter( 'snow_monkey_template_part_root_hierarchy', [ $this, '_template_part_root_hierarchy' ], 10, 2 );
-		add_filter( 'snow_monkey_pre_template_part_render_template-parts/nav/footer-sticky', '__return_false' );
+		add_filter( 'snow_monkey_pre_template_part_render_template-parts/nav/footer-sticky', '__return_false', 9 );
 		add_action( 'wp_footer', [ $this, '_display_footer_cta' ] );
 	}
 
@@ -66,11 +78,9 @@ class Front {
 	 * @param string $slug
 	 * @return string
 	 */
-	public function _template_part_root_hierarchy( $hierarchy, $slug ) {
-		if ( 'footer-cta' === $slug || 0 === strpos( $slug, 'footer-cta/' ) ) {
-			$hierarchy[] = SNOW_MONKEY_FOOTER_CTA_PATH . '/templates';
-			$hierarchy   = array_unique( $hierarchy );
-		}
+	public function _template_part_root_hierarchy( $hierarchy ) {
+		$hierarchy[] = SNOW_MONKEY_FOOTER_CTA_PATH . '/templates';
+		$hierarchy   = array_unique( $hierarchy );
 		return $hierarchy;
 	}
 
